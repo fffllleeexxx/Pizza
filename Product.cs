f987712ae4498d0fcaf98b54310744011d18f466
Эмиль Сайдашев <emilsaidasev@gmail.com>
2024-05-04 21:14:20 +0300

    Добавьте файлы проекта.

---
+++ Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Pizza
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public decimal Price { get; set; }
+    }
+}
